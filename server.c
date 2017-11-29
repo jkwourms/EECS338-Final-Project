2008db07971a133eba947e623020d9dc12258113
jkwourms <jkw64@case.edu>
2017-11-29 09:49:27 -0500

    Update server.c
    Cleaned it up. Made the thread start to work, but we still need to clear the buffer after an order is received and a thread is launched.

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 #include <pthread.h>
 #define BUFFER_SIZE 256
 
-void *chef();
+void *chef(void *);
 
 void error(const char *msg) {
     perror(msg);
@@ -19,16 +19,16 @@ void error(const char *msg) {
 
 int main(int argc, char *argv[]) {
 	int sockfd, newsockfd, portnum;
-	socklen_t custlen;
 	char buffer[BUFFER_SIZE];
+	socklen_t custlen;
 	struct sockaddr_in serv_addr, cust_addr;
-	int order;
 	pthread_t tid;
-
 	if (argc < 2) {
 		fprintf(stderr, "ERROR: did not provide port number");
 		exit(1);
 	}
+
+	//Create Socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR: opening socket");
@@ -39,38 +39,51 @@ int main(int argc, char *argv[]) {
 	serv_addr.sin_port = htons(portnum);
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR: binding");
+
+	//Listen for connection
 	listen(sockfd, 5);
 	custlen = sizeof(cust_addr);
-	newsockfd = accept(sockfd, (struct sockaddr *) &cust_addr, &custlen);
-	if (newsockfd < 0)
-		error("ERROR: accept");
-	bzero(buffer, BUFFER_SIZE);
 
-	//continue to read orders until number of customers is 0
-	
-	pthread_create(&tid, NULL, chef, NULL); //launch chef thread
-	
+	//Accept connection
+	while (newsockfd = accept(sockfd, (struct sockaddr *) &cust_addr, &custlen)) {
+		if (pthread_create(&tid, NULL, chef, &newsockfd) < 0)
+			error("ERROR: could not create thread");
+	}
+
+	//Close sockets & join threads
 	close(newsockfd);
 	close(sockfd);
 	pthread_join(tid, NULL);
 	return 0;
 }
 
-void *chef() {
+void *chef(void *sockfd) {
 	
-	order = read(newsockfd, buffer, BUFFER_SIZE-1);
-        if (order < 0)
-                error("ERROR: reading from socket");
-	
-	printf("Your order was : %s\n", buffer);
-	order = write(newsockfd, "I got your order", 16);
-	if (order < 0)
-		error("ERROR: writing to socket");
-	if (strcmp(order, "cheese")) {
-		printf("sleeping...")
-		sleep(2);
-		printf("awake!");
+	//Get socket descriptor
+	int sock = *(int*)sockfd;
+	int read_size;
+	char *order, customer_order[100];
+
+	//Send message to the customer
+	order = "Please place your order: ";
+	write(sock, order, strlen(order));
+
+	//Receive message from customer
+	while ((read_size = recv(sock, customer_order, 100, 0)) > 0) {
+		//End of string marker
+		customer_order[read_size] = '\0';
+		//Send message back to customer
+		write(sock, customer_order, strlen(customer_order));
+		//Clear message buffer
+		//bzero(buffer, 100)
 	}
+
+	if (read_size == 0) {
+		printf("Thank you, come again!");
+		fflush(stdout);
+	}
+
+   	close(sock);
 	pthread_exit(0);
 
 }
