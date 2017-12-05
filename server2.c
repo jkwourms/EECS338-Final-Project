66cc22268598c80c226db598360ad9750c2f172d
jkwourms <jkw64@case.edu>
2017-12-05 10:46:01 -0500

    Update server2.c

--- server2.c
+++ server2.c
@@ -9,11 +9,11 @@
 #include<pthread.h> 
  
 //the thread function
-void *connection_handler(void *);
+void *chef(void *);
 
 //struct
 struct order_struct {
-    pthread_t tid[3]; //not assignable?
+    pthread_t tid[3]; 
     int order_sock;
 };
  
@@ -21,6 +21,10 @@ int main(int argc , char *argv[])
 {
     int socket_desc , client_sock , c;
     struct sockaddr_in server , client;
+    pthread_t thread_id[3]; 
+    int thread_num = 0;
+    char client_message[2000];
+    struct order_struct args;
      
     //Create socket
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -33,7 +37,7 @@ int main(int argc , char *argv[])
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8000 );
+    server.sin_port = htons( 8888 );
      
     //Bind
     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
@@ -51,27 +55,23 @@ int main(int argc , char *argv[])
     puts("Waiting for hungry customers...");
     c = sizeof(struct sockaddr_in);
     
-    pthread_t thread_id[3]; 
-    int bytes, thread_num = 0;
-    char client_message[2000];
-    struct order_struct args;
-    
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
         puts("Connection accepted");
         args.order_sock = client_sock;
         args.tid[thread_num] = thread_id[thread_num];
          
-        if( pthread_create( &thread_id[thread_num] , NULL ,  connection_handler , (void*) &args) < 0)
+        //needs to be called everytime the client submits a request
+        if( pthread_create( &thread_id[thread_num] , NULL ,  chef , (void*) &args) < 0)
         {
             perror("could not create thread");
             return 1;
         }
         thread_num++;
-        sleep(2); //this is 
+        //printf("thread_num : %d\n", thread_num);
+        //sleep(2);
 
-        
-        puts("Handler assigned");
+        puts("Chef assigned");
     }
      
     if (client_sock < 0)
@@ -80,18 +80,20 @@ int main(int argc , char *argv[])
         return 1;
     }
 
+    close(client_sock);
+
     return 0;
 }
  
 /*
  * This will handle connection for each client
  * */
-void *connection_handler(void *arguments)
+void *chef(void *arguments)
 {
 
     struct order_struct *args = arguments;
     int sock = args->order_sock;
-    int *thread = (int*)args->tid;
+    int thread = *(int*)args->tid;
 
     //Get the socket descriptor
     //int sock = *(int*)socket_desc;
@@ -99,7 +101,6 @@ void *connection_handler(void *arguments)
     char *message , client_message[2000], *toppings, *invalid;
     char all_orders[5000];
     int order_number = 1;
-    int i = 0;
     
     //toppings = "Topping choices are: banana, apple, granola, strawberry \n"; 
     //write(sock, toppings, strlen(toppings));
@@ -107,8 +108,8 @@ void *connection_handler(void *arguments)
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
     {
-        printf("THREAD LAUNCHED : %d\n", thread[i]);
-        fflush(stdout);
+        //printf("THREAD LAUNCHED : %d\n", thread);
+        //fflush(stdout);
         
         if(strcmp(client_message, "blueberries\n") == 0){
 
@@ -127,7 +128,7 @@ void *connection_handler(void *arguments)
             sleep(2);
 
             //print when finished
-            printf("Chef #%d has finished preparing order #%d!\n", thread[i], order_number);
+            printf("Chef has finished preparing order #%d!\n", order_number);
         }
         else if(strcmp(client_message, "banana\n") == 0){
             
@@ -146,7 +147,7 @@ void *connection_handler(void *arguments)
             sleep(1);
 
             //print when finished
-            printf("Chef #%d has finished preparing order #%d!\n", thread[i], order_number);
+            printf("Chef #%d has finished preparing order #%d!\n", thread, order_number);
         }
         else if(strcmp(client_message, "apple\n") == 0){
             //end of string marker
@@ -164,7 +165,7 @@ void *connection_handler(void *arguments)
             sleep(4);
 
             //print when finished
-            printf("Chef #%d has finished preparing order #%d!\n", thread[i], order_number);
+            printf("Chef #%d has finished preparing order #%d!\n", thread, order_number);
         }
         else if(strcmp(client_message, "granola\n") == 0){
             //end of string marker
@@ -182,9 +183,9 @@ void *connection_handler(void *arguments)
             sleep(5);
 
             //print when finished
-            printf("Chef #%d has finished preparing order #%d!\n", thread[i], order_number);
+            printf("Chef #%d has finished preparing order #%d!\n", thread, order_number);
         }
-        else if(strcmp(client_message, "strawberry\n") == 0){
+        else if(strcmp(client_message, "strawberries\n") == 0){
             //end of string marker
             client_message[read_size] = '\0';
             
@@ -200,29 +201,30 @@ void *connection_handler(void *arguments)
             sleep(1);
 
             //print when finished
-            printf("Chef #%d has finished preparing order #%d!\n", thread[i], order_number);
+            printf("Chef #%d has finished preparing order #%d!\n", thread, order_number);
         }
         else{
             invalid = "That's not an option, next! \n";
             write(sock, invalid, strlen(invalid));
             memset(client_message, 0, 2000);
         }
-        i++;
         order_number++;
     }
      
     if(read_size == 0)
     {
-        //write(sock, all_orders, strlen(all_orders));
         printf("%s \n", all_orders);
         puts("Client disconnected");
         fflush(stdout);
+        //not exiting?
+        close(sock);
+        pthread_exit(0);
+        return 0;
     }
     else if(read_size == -1)
     {
         perror("recv failed");
     }
          
-    pthread_exit(NULL); 
     return 0;
 } 
